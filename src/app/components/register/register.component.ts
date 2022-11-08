96b46c1dc6402f133f3a75c8105257a3d1bab60d
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-08 16:39:08 -0300

    cpf validation and added selects

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -45,6 +45,32 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {}
 
   register(): void {
-    console.log(this.form.value);
+    console.log(this.form);
+  }
+
+  cpfChange(): void {
+    let cpfValue: string = this.form.value.cpf;
+    let numeric = cpfValue.replace(/[^0-9]+/g, '');
+    let cpfLength = numeric.length;
+
+    let partOne = numeric.slice(0, 3) + '.';
+    let partTwo = numeric.slice(3, 6) + '.';
+    let partThree = numeric.slice(6, 9) + '-';
+
+    if (cpfLength && cpfLength < 4) {
+      this.form.controls['cpf'].setValue(numeric);
+    } else if (cpfLength >= 4 && cpfLength < 7) {
+      let formatCPF = partOne + numeric.slice(3);
+      this.form.controls['cpf'].setValue(formatCPF);
+    } else if (cpfLength >= 7 && cpfLength < 10) {
+      let formatCPF = partOne + partTwo + numeric.slice(6);
+      this.form.controls['cpf'].setValue(formatCPF);
+    } else if (cpfLength >= 10 && cpfLength < 12) {
+      let formatCPF = partOne + partTwo + partThree + numeric.slice(9);
+      this.form.controls['cpf'].setValue(formatCPF);
+    } else if (cpfLength >= 12) {
+      let formatCPF = partOne + partTwo + partThree + numeric.slice(9, 11);
+      this.form.controls['cpf'].setValue(formatCPF);
+    }
   }
 }

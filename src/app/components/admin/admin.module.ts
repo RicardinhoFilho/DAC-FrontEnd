6be38b034bdb52bba6f23e435057c34196f73590
felipekbm <felipekazuo05@hotmail.com>
2022-11-15 17:48:44 -0300

    [add] componentes admin

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminRelatorioComponent } from './admin-relatorio/admin-relatorio.component';
+import { AdminAdicionarGerenteComponent } from './admin-adicionar-gerente/admin-adicionar-gerente.component';
+import { AdminRemoverGerenteComponent } from './admin-remover-gerente/admin-remover-gerente.component';
+import { AdminListarGerenteComponent } from './admin-listar-gerente/admin-listar-gerente.component';
+import { AdminEditarGerenteComponent } from './admin-editar-gerente/admin-editar-gerente.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminHomeComponent
+    AdminHomeComponent,
+    AdminRelatorioComponent,
+    AdminAdicionarGerenteComponent,
+    AdminRemoverGerenteComponent,
+    AdminListarGerenteComponent,
+    AdminEditarGerenteComponent
   ],
   imports: [
     CommonModule

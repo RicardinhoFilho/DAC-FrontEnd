7bdc4c04f3dc94a291fc6e6744149a057b533021
felipekbm <felipekazuo05@hotmail.com>
2022-11-26 21:36:09 -0300

    [fix] diretiva numerico

--- src/app/components/admin/admin.module.ts
+++ src/app/components/admin/admin.module.ts
@@ -20,7 +20,7 @@ import { NumericoDirective } from '@shared/directives/numerico.directive';
     AdminRemoverGerenteComponent,
     AdminListarGerenteComponent,
     AdminEditarGerenteComponent,
-    NumericoDirective
+    // NumericoDirective
   ],
   imports: [
     CommonModule,

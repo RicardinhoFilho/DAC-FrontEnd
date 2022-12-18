d6e1203b55ba8ef351c5d5976d0838016672de8e
felipekbm <felipekazuo05@hotmail.com>
2022-12-18 16:00:35 -0300

    listar gerentes alterado para material

--- src/app/components/admin/admin-listar-gerente/admin-listar-gerente.component.ts
+++ src/app/components/admin/admin-listar-gerente/admin-listar-gerente.component.ts
@@ -1,33 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '@components/auth/services/user.service';
+import { lastValueFrom } from 'rxjs';
 import { User } from './../../../shared/models/user.model';
 
+interface AdminGerentesTable {
+  user: User;
+}
 @Component({
   selector: 'app-admin-listar-gerente',
   templateUrl: './admin-listar-gerente.component.html',
   styleUrls: ['./admin-listar-gerente.component.scss'],
 })
 export class AdminListarGerenteComponent implements OnInit {
-  gerentes: User[] = [];
+  adminGerentesTable: AdminGerentesTable[] = [];
 
-  constructor(private userService: UserService) {}
+  displayedColumns = [
+    'nomeGerente',
+    'email',
+    'cpf',
+    'telefone',
+  ];
+  dataSource!: MatTableDataSource<AdminGerentesTable>;
 
-  ngOnInit(): void {
-    this.listarTodos();
-  }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  private listarTodos(): void {
-    this.userService.getGerentes().subscribe((users: User[]) => {
-      this.gerentes = users;
+  constructor(
+    private userService: UserService,
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    this.userService.getGerentes().subscribe(async (users: User[]) => {
+      await Promise.all(
+        users.map(async (user: User): Promise<void> => {
+          this.adminGerentesTable.push({
+            user: user,
+          });
+        })
+      );
+
+      this.dataSource = new MatTableDataSource(this.adminGerentesTable);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.dataSource.sortingDataAccessor = (
+        item: AdminGerentesTable,
+        property: string
+      ) => {
+        switch (property) {
+          case 'nomeGerente':
+            return item.user.nome;
+          case 'email':
+            return item.user.email;
+          case 'cpf':
+            return item.user.cpf;
+          case 'telefone':
+            return item.user.telefone;
+          default:
+            return (item as any)[property];
+        }
+      };
+      const sortState: Sort = { active: 'nomeGerente', direction: 'asc' };
+      this.sort.direction = sortState.direction;
+      this.sort.sortChange.emit(sortState);
     });
-  }
-
-  remover($event: any, gerente: User): void {
-    $event.preventDefault();
-    if (confirm(`Remover ${gerente.nome}?`)) {
-      this.userService.remover(gerente.id!).subscribe(() => {
-        this.listarTodos();
-      });
-    }
   }
 }
